22d00d8d1a21a86f8b136a4ba7bbaa2a96d09446
Weslley <weslleyothiago@gmail.com>
2024-08-06 19:34:46 -0700

    Novo layout da pagina de login

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'funky-bubble': ['"Funky Bubble"', 'sans-serif']
+      }
+    }
   },
   plugins: [],
 }
-
